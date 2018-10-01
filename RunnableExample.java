3d4d9606fd1e3829d78808fc880613a5c03489a2
Joshua Chen <joshuachen1@cpp.edu>
2018-09-30 23:09:03 -0700

    second commit (Using Lambda Expressions for Threads)

--- RunnableExample.java
+++ RunnableExample.java
@@ -12,5 +12,11 @@ public class RunnableExample {
         });
 
         myThread.run();
+
+        // Since Runnable has a single method and Lambda Expressions work like Anonymous Inner Classes
+        // We can create a Lambda Expression for this thread
+
+        Thread myLambdaThread = new Thread(() -> System.out.println("Printed inside Lambda Runnable"));
+        myLambdaThread.run();
     }
 }
