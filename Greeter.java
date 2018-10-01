061ea1202ebf96d24a57ca0b32e702d1bb5833af
Joshua Chen <joshuachen1@cpp.edu>
2018-09-30 19:46:21 -0700

    first commit

---
+++ Greeter.java
@@ -0,0 +1,14 @@
+/**
+ * Created by student on 9/30/18.
+ */
+public class Greeter {
+
+    public void greet() {
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        greeter.greet();
+    }
+}
