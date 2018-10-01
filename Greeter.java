178639afff6b32643ed710d6a65fe12ad21e07af
Joshua Chen <joshuachen1@cpp.edu>
2018-09-30 22:41:39 -0700

    fifth commit

--- Greeter.java
+++ Greeter.java
@@ -8,14 +8,6 @@ public class Greeter {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
 
-        Greeting helloWorldGreeting = new HelloWorldGreeting();
-
-        // The interface type of the Lambda Expression may only contain one method
-        // The lambda expression's signature must match the signature of the functional interface
-        // Since both the lambda expression and the perform method in Greeting
-        // are both void and does not accept any arguments, the lambda expression is valid.
-        Greeting lambdaGreeting = () -> System.out.println("Hello World");
-
         // Inline implementation of an Interface
         // Also known as an Anonymous Inner Class
         Greeting innerClassGreeting = new Greeting() {
@@ -24,8 +16,11 @@ public class Greeter {
             }
         };
 
-        // This is how lambda expressions are executed.
-        // By calling the interface method, it acts as if it were an instance of a class.
-        innerClassGreeting.perform();
+        // Lambda expressions do the exact same thing as an Anonymous Inner Class
+        Greeting lambdaGreeting = () -> System.out.println("Hello World");
+
+        greeter.greet(innerClassGreeting);
+
+        greeter.greet(lambdaGreeting);
     }
 }
