bcb2295afb59287f10d74997c4852c9737113d17
Joshua Chen <joshuachen1@cpp.edu>
2018-09-30 19:54:06 -0700

    first commit

--- Greeter.java
+++ Greeter.java
@@ -1,14 +1,17 @@
-/**
- * Created by student on 9/30/18.
- */
+
 public class Greeter {
 
-    public void greet() {
-        System.out.println("Hello World");
+    public void greet(Greeting greeting) {
+        greeting.perform();
     }
 
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
-        greeter.greet();
+        HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
+
+        // The greet method accepts a Greeting object
+        // Then calls the Greeting object's perform method
+        // In this case, the HelloWorldGreeting's perform method prints "Hello World!"
+        greeter.greet(helloWorldGreeting);
     }
 }
