af9e6881773f91d0f22e8a94f782811712d37c48
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 00:12:48 -0700

    second commit (Functional Interface)

--- Greeting.java
+++ Greeting.java
@@ -1,6 +1,8 @@
 
 // Labels the interface as a Functional Interface
 // Therefore it can only have 1 abstract method
+// It is completely optional and not required for Lambda types
+// But is good practice
 @FunctionalInterface
 public interface Greeting {
     public void perform();
