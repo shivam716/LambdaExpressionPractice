de494b3aae3548919b57b97d6606e4d61bf1b5b4
Joshua Chen <joshuachen1@cpp.edu>
2018-10-01 00:12:13 -0700

    second commit (Functional Interface)

--- Greeting.java
+++ Greeting.java
@@ -1,3 +1,7 @@
+
+// Labels the interface as a Functional Interface
+// Therefore it can only have 1 abstract method
+@FunctionalInterface
 public interface Greeting {
     public void perform();
 }
