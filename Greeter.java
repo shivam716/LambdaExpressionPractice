707754df2b0e01e2794a5cc7a9eda36f25ab2f0e
Joshua Chen <joshuachen1@cpp.edu>
2018-09-30 22:50:01 -0700

    sixth commit (Passing lambda expression into methods)

--- Greeter.java
+++ Greeter.java
@@ -8,19 +8,11 @@ public class Greeter {
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
 
-        // Inline implementation of an Interface
-        // Also known as an Anonymous Inner Class
-        Greeting innerClassGreeting = new Greeting() {
-            public void perform() {
-                System.out.println("Hello World!");
-            }
-        };
-
-        // Lambda expressions do the exact same thing as an Anonymous Inner Class
-        Greeting lambdaGreeting = () -> System.out.println("Hello World");
-
-        greeter.greet(innerClassGreeting);
-
-        greeter.greet(lambdaGreeting);
+        // Since the greet method accepts an instance of Greeting
+        // and the function () -> System.out.println("Hello World") can be treated as a value
+        // then the lambda expression can be passed as an argument
+        // if the lambda expression's signature matches that of the instance it derives from
+        // (in this case: the signature matches the signature of Greeter's perform method
+        greeter.greet(() -> System.out.println("Hello World"));
     }
 }
