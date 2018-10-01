c550b61aaa73247fe7cd5b406ae309defe841ca9
Joshua Chen <joshuachen1@cpp.edu>
2018-09-30 20:58:32 -0700

    third commit

--- Greeter.java
+++ Greeter.java
@@ -7,11 +7,20 @@ public class Greeter {
 
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
-        HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
 
-        // The greet method accepts a Greeting object
-        // Then calls the Greeting object's perform method
-        // In this case, the HelloWorldGreeting's perform method prints "Hello World!"
-        greeter.greet(helloWorldGreeting);
+        Greeting helloWorldGreeting = new HelloWorldGreeting();
+
+        // The interface type of the Lambda Expression may only contain one method
+        // The lambda expression's signature must match the signature of the functional interface
+        // Since both the lambda expression and the perform method in Greeting
+        // are both void and does not accept any arguments, the lambda expression is valid.
+        Greeting lambdaGreeting = () -> System.out.println("Hello World");
+
+        // Basic object-oriented programming where the HelloWorldGreeting object calls the perform method
+        helloWorldGreeting.perform();
+
+        // This is how lambda expressions are executed.
+        // By calling the interface method, it acts as if it were an instance of a class.
+        lambdaGreeting.perform();
     }
 }
