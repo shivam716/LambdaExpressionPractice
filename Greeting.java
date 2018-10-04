51e71117007431cd5381550790c445841f8efe47
shivam716 <38368417+shivam716@users.noreply.github.com>
2018-10-04 22:17:08 +0530

    added a comment.
    added information about use of default methods.

--- Greeting.java
+++ Greeting.java
@@ -3,6 +3,7 @@
 // Therefore it can only have 1 abstract method
 // It is completely optional and not required for Lambda types
 // But is good practice
+// Java 8 onwards we can use default methods within in Functional interface with a condition that there must exist exactly one abstract method 
 @FunctionalInterface
 public interface Greeting {
     public void perform();
